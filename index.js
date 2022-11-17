0c11aefc04fea927c06f6042c3bddcecbf13aeab
GiselleBarbosa <g88barbosa@gmail.com>
2022-11-16 22:18:06 -0300

    update port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
+const port = process.env.PORT || 3000
 const products = require('./src/data/products.json');
 
 server.get('/products', (req, res) => {
   return res.json(products)
 })
 
-server.listen(3000,() =>{
+server.listen(port,() =>{
   console.log("Server works!!!");
 })
